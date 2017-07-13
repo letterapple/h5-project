337f78bffc1b1d2c631f7da0f9a6d641a3d870b1
杜萍萍 <dupingping@eefung.com>
2017-07-13 16:12:07 +0800

    项目中添加less。

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -32,7 +32,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 h1, h2 {
   font-weight: normal;
 }
@@ -40,14 +40,14 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    a {
+      color: #238ab9;
+    }
+  }
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 
-a {
-  color: #42b983;
-}
 </style>
